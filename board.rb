1d0a8fdc4554af7e4606ca414e63e8853b763d1e
Vincent Klimjack <vklimjack@gmail.com>
2020-07-04 12:38:45 +0100

    added separate files for game, board, and player classes

---
+++ board.rb
@@ -0,0 +1,28 @@
+require_relative "player"
+
+class Board
+
+  attr_reader :game_board
+  
+  def initialize
+    @game_board = create_board
+  end
+
+  def display_board
+    puts
+    puts <<~HEREDOC
+     #{game_board[0]} | #{game_board[1]} | #{game_board[2]}
+    ---+---+---
+     #{game_board[3]} | #{game_board[4]} | #{game_board[5]}
+    ---+---+---
+     #{game_board[6]} | #{game_board[7]} | #{game_board[8]}
+    HEREDOC
+    puts
+  end
+
+  private
+
+  def create_board
+    Array.new(9) { |cell| (cell + 1). to_s }
+  end
+end
--- game.rb
+++ game.rb
@@ -1,29 +1,8 @@
-class Board
+require_relative "board"
+require_relative "player"
 
-  attr_reader :game_board
-  
-  def initialize
-    @game_board = create_board
-  end
+class Game
 
-  def display_board
-    puts
-    puts <<~HEREDOC
-     #{game_board[0]} | #{game_board[1]} | #{game_board[2]}
-    ---+---+---
-     #{game_board[3]} | #{game_board[4]} | #{game_board[5]}
-    ---+---+---
-     #{game_board[6]} | #{game_board[7]} | #{game_board[8]}
-    HEREDOC
-    puts
-  end
 
-  private
-
-  def create_board
-    Array.new(9) { |cell| (cell + 1). to_s }
-  end
 end
 
-b = Board.new
-b.display_board
