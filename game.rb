98243baccb9504a11735c359a727b58e68a4e210
Vincent Klimjack <vklimjack@gmail.com>
2020-07-08 14:29:46 +0100

    fixed typo in win condtions

--- game.rb
+++ game.rb
@@ -10,7 +10,7 @@ class Game
     [0, 1, 2], [3, 4, 5],
     [6, 7, 8], [0, 3, 6],
     [1, 4, 7], [2, 5, 8],
-    [0, 3, 8], [2, 4, 6]
+    [0, 4, 8], [2, 4, 6]
   ]
 
   def initialize
@@ -37,7 +37,7 @@ class Game
   end
 
   private
-  
+
   def instructions
     puts <<-RULES
 
