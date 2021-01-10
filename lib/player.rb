c17d39e1196cf7b3988a75c14b14f61632f8f62f
Vincent Klimjack <vklimjack@gmail.com>
2021-01-10 21:11:34 +0000

    modified player class to read easier, and added two tests for #set_marker#

--- lib/player.rb
+++ lib/player.rb
@@ -6,19 +6,23 @@ class Player
     @marker = set_marker
   end
 
-  private
-
   def set_marker
     print "Player #{player_number}! Please choose a letter to indicate your marker: "
-    chosen_marker = gets.chomp
-    validate_marker(chosen_marker)
+    loop do
+      @marker = validate_marker(player_input)
+      break if @marker
+
+      puts 'Please enter a single letter, a-z!'
+    end
   end
 
+  private
+
   def validate_marker(chosen_marker)
-    until chosen_marker.match?(/[a-zA-Z]/)
-      print 'Please enter a letter: '
-      chosen_marker = gets.chomp
-    end
-    chosen_marker.upcase
+    return chosen_marker.upcase if chosen_marker.match?(/[a-zA-Z]/)
+  end
+
+  def player_input
+    gets.chomp
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,4 +3,29 @@
 require './lib/player'
 
 describe Player do
+  subject(:player_one) { described_class.new(1) }
+
+  describe '#set_marker' do
+    context 'when user inputs a valid marker' do
+      it 'stops the loop and does not display an error message' do
+        valid_input = 'x'
+        allow(player_one).to receive(:player_input).and_return(valid_input)
+        expect(player_one).not_to receive(:puts).with('Please enter a single letter, a-z!')
+        player_one.set_marker
+      end
+    end
+
+    context 'when user inputs an invalid marker, then a valid marker' do
+      before do
+        num = '9'
+        valid_input = 'x'
+        allow(player_one).to receive(:player_input).and_return(num, valid_input)
+      end
+
+      it 'completes loop and displays error message once' do
+        expect(player_one).to receive(:puts).with('Please enter a single letter, a-z!').once
+        player_one.set_marker
+      end
+    end
+  end
 end
